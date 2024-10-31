2a5d6dd21d25f2f4e5d4b26f60229dbc4a95ebb6
Sahib Singh <sahaibsingh001@live.com>
2024-10-31 17:49:27 +0530

    Fix canonical URL and site configuration

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
-import node from '@astrojs/node'
-
 export default defineConfig({
   output: "server",
   adapter: node({
     mode: "standalone"
   }),
+  site: process.env.HEROKU_APP_NAME 
+    ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
+    : 'https://sahaib-portfolio-9ba1759077cd.herokuapp.com',
   integrations: [
     react(),
     tailwind({
