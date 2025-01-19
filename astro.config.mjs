95038ff5a8b21f21b68422c512368ab9cd6d2da7
Sahib Singh <sahaibsingh001@live.com>
2025-01-19 13:44:04 +0530

    Switch to static output for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  output: 'static',
   site: 'https://work.sahaibsingh.com',
   integrations: [react(), tailwind()]
 }); 
