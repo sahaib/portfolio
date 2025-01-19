c1188dbe17ef4e84ec8d8bdb02253f78d3e07d48
Sahib Singh <sahaibsingh001@live.com>
2025-01-19 16:19:51 +0530

    updated portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-    runtime: 'nodejs20.x'
-  }),
+  output: 'static',
   site: 'https://work.sahaibsingh.com',
   integrations: [react(), tailwind()]
 }); 
