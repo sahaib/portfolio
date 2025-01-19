1485e7c5e3de0ee456546871ae254c89491597b0
Sahib Singh <sahaibsingh001@live.com>
2025-01-19 16:24:02 +0530

    Fix build issues and content revalidation

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,11 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   output: 'static',
   site: 'https://work.sahaibsingh.com',
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  vite: {
+    build: {
+      // Force clean build every time
+      emptyOutDir: true,
+    },
+  }
 }); 
