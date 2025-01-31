9c98c839e69788055393c3e270247e5c045440e9
Sahaib Singh <sahaibsingh001@live.com>
2025-01-31 19:42:06 +0530

    updated contentful models

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,4 +5,9 @@ interface ImportMetaEnv {
   readonly CONTENTFUL_SPACE_ID: string
   readonly CONTENTFUL_DELIVERY_TOKEN: string
   readonly CONTENTFUL_PREVIEW_TOKEN: string
+  readonly DEV: boolean
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
 }
