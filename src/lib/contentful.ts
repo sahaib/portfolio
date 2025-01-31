4480e6722c280d15061962df8a72ec071d13e872
Sahaib Singh <sahaibsingh001@live.com>
2025-01-31 19:37:10 +0530

    updated models

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,14 +1,21 @@
-import * as contentful from 'contentful'
+import contentful from 'contentful';
+const { createClient } = contentful;
 
-export const contentfulClient = contentful.createClient({
+export const contentfulClient = createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
   accessToken: import.meta.env.DEV
     ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
     : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
   host: import.meta.env.DEV ? 'preview.contentful.com' : 'cdn.contentful.com',
 })
-const entries = await contentfulClient.getEntries({
-  content_type: 'projects',
-  order: ['-sys.createdAt'],
-})
-console.log(entries)
+
+// To get only Projects content type (excluding LinkedIn Articles)
+export async function getProjects() {
+  const entries = await contentfulClient.getEntries({
+    content_type: 'projects',
+    order: ['-sys.createdAt']
+  })
+  
+  return entries
+}
+
