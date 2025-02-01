ff06edae9ba2a4c4c187527e11127008bf07955a
Sahaib Singh <sahaibsingh001@live.com>
2025-02-01 13:46:47 +0530

    made contentful realtime

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -2,18 +2,15 @@ import * as contentful from 'contentful';
 
 export const contentfulClient = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
-  accessToken: import.meta.env.DEV
-    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
-    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
-  host: import.meta.env.DEV ? 'preview.contentful.com' : 'cdn.contentful.com',
+  accessToken: import.meta.env.CONTENTFUL_PREVIEW_TOKEN,
+  host: 'preview.contentful.com',
 })
 
 // To get only Projects content type (excluding LinkedIn Articles)
 export async function getProjects() {
   const entries = await contentfulClient.getEntries({
     content_type: 'projects',
-    order: ['-sys.createdAt'],
-    limit: 100
+    order: ['-sys.createdAt']
   })
   
   return entries
